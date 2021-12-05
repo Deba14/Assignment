32d86d593f45fbd5f4228897603713e2ba911651
Debashis Sahoo <93909482+Deba14@users.noreply.github.com>
2021-12-05 21:20:22 +0530

    Add files via upload

---
+++ lambda sum.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+#Write a Python program to create a lambda function that adds 25 to a given number passed in as an argument.
+
+
+
+sample input: 10
+
+sample output: 35
+
+
+# In[1]:
+
+
+fun1 = lambda x: x+25
+
+
+# In[2]:
+
+
+print(fun1(10))
+
+
+# In[ ]:
+
+
+
+
